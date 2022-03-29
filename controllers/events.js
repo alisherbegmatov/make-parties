19fd46eea93de0ddfb32ea77b0a99f098b625753
alisherbegmatov <alisher.begmatov@students.dominican.edu>
2022-03-28 22:37:03 -0700

    Challenge 10 Completed
    Challenge 10 Completed

--- controllers/events.js
+++ controllers/events.js
@@ -1,5 +1,6 @@
-//events.js
+const moment = require("moment")
 
+//events.js
 module.exports = function (app, models) {
 
     // INDEX
@@ -26,6 +27,9 @@ module.exports = function (app, models) {
     // SHOW
     app.get('/events/:id', (req, res) => {
         models.Event.findByPk(req.params.id, { include: [{ model: models.Rsvp }] }).then(event => {
+            let createdAt = event.createdAt;
+            createdAt = moment(createdAt).format('MMMM Do YYYY, h:mm:ss a');
+            event.createdAtFormatted = createdAt;
             res.render('events-show', { event: event });
         }).catch((err) => {
             console.log(err.message);
